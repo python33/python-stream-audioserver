985a4297ec0300c657921bc4ab02a1c31622a02a
Jørgen Grimnes <jorgenkg@gmail.com>
2013-01-25 13:15:11 +0100

    Minor: ability to play sounds
    Currently playing sounds incorrectly
    
    Other:
     - added audioread
     - requires portaudio to run

--- client.py
+++ client.py
@@ -2,5 +2,4 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.sendto("test\n", ('', 8080))
-print "Received: {}".format(sock.recv(1024))
+sock.sendto("test\n", ('', 8080)) # just to instatiate a handler on the server
--- server.py
+++ server.py
@@ -1,28 +1,36 @@
 from __future__ import with_statement
 import audioread
+import pyaudio
 import os
 from SocketServer import ThreadingMixIn, UDPServer, BaseRequestHandler
+
 
 class RequestHandler( BaseRequestHandler ):
     
     def __init__(self, request, client_address, server):
         BaseRequestHandler.__init__(self, request, client_address, server)
-    
-    def _open_file(self, filename):
-        filename = os.path.abspath(os.path.expanduser(filename))
-        try:
-            return audioread.audio_open(filename)
-        except Exception, e:
-            print "File doesnt exist: %s" % filename
-            return False
+        
     
     def handle(self):
-        f = self._open_file("/Users/Jorgenkg/Desktop/sheep.mp3")
-        if f:
-            print 'Input file: %i channels at %i Hz; %.1f seconds.' % (f.channels, f.samplerate, f.duration)
-            f.read_data()
-            f.close()
         recieved_data, connection = self.request
+        self._audiorender = pyaudio.PyAudio()
+        
+        f = audioread.audio_open(os.path.join(os.path.dirname(__file__),"test.mp3"))
+        
+        stream = self._audiorender.open(
+                                        format=pyaudio.paInt16,
+                                        channels=f.channels,
+                                        rate=f.samplerate,
+                                        output=True
+                                    )
+        for buff in f:
+            stream.write(buff)
+        f.close()
+        stream.stop_stream()
+        stream.close()
+        
+        self._audiorender.terminate()
+        
         connection.sendto(recieved_data.upper(), self.client_address)
     
 class StreamServer(UDPServer):
