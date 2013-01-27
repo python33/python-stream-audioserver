e152c94e4b9dedefb6cd27eedd16f0782cd25877
Jørgen Grimnes <jorgenkg@gmail.com>
2013-01-27 12:33:27 +0100

    Major: runtime signals between components
    Detail:
     - Server always listens to signals from client
     - Both parties may request a shutdown
     - Sane exception handling
     - Closer to implementing the acctual streaming capability
     - Sane DEBUG output to stdout
     - New test sound, 'cause the previous were maddening

--- client.py
+++ client.py
@@ -1,9 +1,44 @@
 import socket
+import select
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setblocking(1)
+DEBUG = True
+CONNECTION = ('', 8080)
 
-sock.sendto("test\n", ('', 8080))
-
-sock.recv(1024) # equals a wait
-print "Sound is played."
+class AudioSocket( socket.socket ):
+    
+    def __init__(self, family=socket.AF_INET, type=socket.SOCK_STREAM):
+        socket.socket.__init__(self, family, type)
+        try:
+            self.connect( CONNECTION )
+            if DEBUG: print "* added to server stream"
+        except Exception:
+            print "ERROR: not able to connect to server"
+    
+    def _wait_response(self, block_size = 1024):
+        ready = select.select([self], [], [], 30*60)
+        if ready[0]:
+            return self.recv(block_size)
+    
+    def _do_action(self, msg):
+        if 'stop' == msg.lower():
+            if DEBUG: print "* Server signaled shutdown"
+            self.run = False
+            self.close()
+        elif 'playing' == msg.lower(): 
+            self.play()
+    
+    def play(self): pass
+    
+    def start(self):
+        self.run = True
+        
+        try:
+            while self.run:
+                self._do_action( self._wait_response() )
+        except KeyboardInterrupt:
+            if DEBUG: print "* Disconnected from host"
+            self.sendall("bye")
+            self.close()
+        
+a = AudioSocket()
+a.start()
--- server.py
+++ server.py
@@ -1,24 +1,89 @@
-from __future__ import with_statement
 import audioread
 import pyaudio
 import os
-from SocketServer import ThreadingMixIn, UDPServer, BaseRequestHandler
+import socket
+from SocketServer import ThreadingMixIn, BaseRequestHandler,\
+    TCPServer
+import threading
+import select
 
+
+DEBUG = True
 
 class RequestHandler( BaseRequestHandler ):
 
     def __init__(self, request, client_address, server):
         BaseRequestHandler.__init__(self, request, client_address, server)
-        
-    def _open_file(self, filename):
-        self._file = audioread.audio_open( filename )
+    
+    def _send(self, msg):
+        self.request.sendall(msg)
+            
+    def _do_action(self, msg):
+        if 'bye' == msg.lower():
+            if DEBUG: print "* disconnecting sink: %s:%d" % self.request.getsockname()
+            self.run = False
+            self.server.remove_audiosink( self.request )
+        else: 
+            return False
+        return True
+    
+    def _wait_response(self, block_size = 1024):
+        ready = select.select([self.request], [], [], 30*60)
+        if ready[0]:
+            return self.recv(block_size)
     
     def handle(self):
-        received_data, connection = self.request
+        self.run = True
+        self.server.add_audiosink( self.request )
         
-        self._open_file( os.path.join(os.path.dirname(__file__),"test.mp3") )
+        try:
+            while self.run:
+                if DEBUG: print "* handler waiting: %s:%d" % self.request.getsockname()
+                command = self._wait_response()
+                if self._do_action( command ):
+                    self._send("ok")
+                else:
+                    print "ERROR: unknown command: %s" % command
+                    self._do_action("bye")
+        except Exception:
+            self._do_action("bye")
+    
+class StreamServer(TCPServer):
+    
+    def __init__(self, server_address=('', 8080)):
+        TCPServer.__init__(self,
+                        server_address,
+                        RequestHandler
+                    )
+        self.sinks = [ ]
+        self.is_playing = False
+        self.lock = threading.Lock()
+        
+    def server_close(self):
+        for sink in self.sinks:
+            sink.sendall("stop")
+        
+        TCPServer.server_close(self)
+    
+    def add_audiosink(self, sink):
+        self.lock.acquire()
+        
+        if DEBUG: print "* Add sink: %s:%d" % sink.getsockname()
+        self.sinks.append(sink)
+        self.lock.release()
+        
+        self.play()
+        
+    def remove_audiosink(self, sink):
+        self.lock.acquire()
+        for index, element in enumerate( self.sinks ):
+            if sink == element: del self.sinks[index]
+        if DEBUG: print "* Remove sink: %s:%d" % sink.getsockname()
+        self.lock.release()
+    
+    def _stream_audio(self):
+        self._file = audioread.audio_open( os.path.join(os.path.dirname(__file__), "test.mp3" ))
         self._audiorender = pyaudio.PyAudio()
-        
         self.stream = self._audiorender.open(
                                         format=pyaudio.paInt16,
                                         channels=self._file.channels,
@@ -28,20 +93,22 @@ class RequestHandler( BaseRequestHandler ):
         for buff in self._file:
             self.stream.write(buff)
         
-        
-        self._file.close()
         self.stream.stop_stream()
         self.stream.close()
         self._audiorender.terminate()
         
-        connection.sendto("1", self.client_address) # signal that we are done
+        self.lock.acquire()
+        self.is_playing = False
+        self.lock.release()
     
-class StreamServer(UDPServer):
-    def __init__(self, server_address=('', 8080)):
-        UDPServer.__init__(self,
-                        server_address,
-                        RequestHandler
-                    )
+    def play(self):
+        self.lock.acquire()
+        if not self.is_playing:
+            self.is_playing = True
+            if DEBUG: print "* Streaming to %d sinks." % len(self.sinks)
+            self.lock.release()
+            
+            self._stream_audio()
 
 class ThreadedStreamServer(ThreadingMixIn, StreamServer):
     pass
